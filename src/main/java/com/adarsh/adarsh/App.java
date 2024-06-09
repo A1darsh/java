9f3099eebf7cde263b8f7098aeac143bab569d54
adarsh <adarshmishra5432@gmail.com>
2024-06-10 02:41:26 +0530

    remove something

--- src/main/java/com/adarsh/adarsh/App.java
+++ src/main/java/com/adarsh/adarsh/App.java
@@ -7,9 +7,7 @@ public class App
     	for(int i = 0 ; i<15; i++) {
     		System.out.println( "Hello!" );
     	}
-        for(int i = 0 ; i<20; i++) {
-    		System.out.println( "Hello World!" );
-    	}
+        
 
         
     }
