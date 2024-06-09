dbf6b267658344087a3992d3bd8d23f65e18e2f1
adarsh <adarshmishra5432@gmail.com>
2024-06-10 02:32:17 +0530

    done

--- src/main/java/com/adarsh/adarsh/App.java
+++ src/main/java/com/adarsh/adarsh/App.java
@@ -4,9 +4,13 @@ public class App
 {
     public static void main( String[] args )
     {
-    	for(int i = 0 ; i<10; i++) {
+    	for(int i = 0 ; i<15; i++) {
+    		System.out.println( "Hello!" );
+    	}
+        for(int i = 0 ; i<20; i++) {
     		System.out.println( "Hello World!" );
     	}
+
         
     }
 }
